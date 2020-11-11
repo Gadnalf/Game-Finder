81424c1dc122393794535579e6d02433170b1c44
Philip <philip.cai.mao@hotmail.ca>
2020-11-11 16:08:58 -0500

    Added steam search page ajax request doodad and parser for ids

--- SteamApi.py
+++ SteamApi.py
@@ -1,5 +1,8 @@
 from typing import List, Dict
+from fake_useragent import UserAgent
 import requests
+import urllib.request
+import re
 
 key = "30042FF9A1CAF4AD66072C779330F394"
 
@@ -41,18 +44,34 @@ def GetUserInfoFromIds(user_ids: List[str]) -> List[Dict[str, str]]:
     response = requests.get(url)
     response.raise_for_status()
 
-    user_info = response.json()['response']['players']
+    user_info = response.json()["response"]["players"]
 
     return user_info
 
-# Doesn't work
 def SearchUsers(search_text: str):
-    url = "https://steamcommunity.com/search/SearchCommunityAjax?text=gadnalf&filter=users&sessionid=42bfd6477c411fcb1dd79f9c&steamid_user=false&page=1"
+    ua = UserAgent()
+    session = requests.Session()
+    session.get("https://steamcommunity.com/search/users/")
+    print(session.headers)
+    print(session.cookies)
+    response = session.get(
+        url="https://steamcommunity.com/search/SearchCommunityAjax?text={0}&filter=users&sessionid={1}&steamid_user=false&page=1".format(search_text, str(session.cookies.get('sessionid'))),
+        headers={
+            "User-Agent": str(ua.chrome),
+            "Referer": "https://steamcommunity.com/search/users/",
+            "X-Requested-With": "XMLHttpRequest"
+        }
+    )
+    return ParseUserSearch(response.json())
 
-    response = requests.get(url)
-    return response
+def ParseUserSearch(search_response: Dict[str, str]):
+    html = search_response["html"]
 
-friends = GetFriendsIds("76561198117539193")
+    custom_ids = re.findall("<a href=\"https://steamcommunity.com/id/(.*?)\">", html)
+    ids = re.findall("<a href=\"https://steamcommunity.com/profiles/(.*?)\">", html)
+    return custom_ids + ids
+
+#friends = GetFriendsIds("76561198117539193")
 #print(GetFriendsIds("peepeepoopoo"))
-print(GetUserInfoFromIds(friends))
-#print(SearchUsers("gadnalf"))
+#print(GetUserInfoFromIds(friends))
+print(SearchUsers("gadnalf"))
